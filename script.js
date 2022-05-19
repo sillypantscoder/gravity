e6b4eb5208042eef2358f3c19f2018adab14c01a
Jason Roman <funjasonroman@gmail.com>
2022-05-19 12:07:27 -0600

    Added comments; added randomized region of planets

--- script.js
+++ script.js
@@ -4,25 +4,46 @@ class Position {
 		this.y = Number(y)
 	}
 }
+/**
+ * A planet on the screen
+ */
 class Planet {
+	/**
+	 * Creates a new planet on the screen.
+	 * @param {Number} x The x position of the planet
+	 * @param {Number} y The y position of the planet
+	 * @param {Number} r The radius of the planet
+	 * @param {Number} mass The mass of the planet
+	 * @param {Number} brightness The brightness of the planet
+	 */
 	constructor(x, y, r, mass, brightness) {
+		// Stats
 		this.pos = new Position(Number(x), Number(y))
 		this.radius = Number(r)
 		this.mass = Number(mass)
 		this.color = Number(brightness)
+		this.velocity = new Position(0, 0)
+		// Element
 		this.elm = document.createElement("div")
 		this.elm.classList.add("planet")
 		this.elm.setAttribute("style", `--top: ${this.pos.y}px; --left: ${this.pos.x}px; --radius: ${this.radius}px; background-color: rgb(${this.color*255}, ${this.color*255}, ${this.color*255});`)
 		document.body.appendChild(this.elm)
-		this.velocity = new Position(0, 0)
+		// Add to scene
 		planets.push(this)
 		if (max_height < this.pos.y + this.radius + 100) {
 			max_height = this.pos.y + this.radius + 100
 		}
 	}
+	/**
+	 * Updates the location of the planet on the screen
+	 */
 	update() {
 		this.elm.setAttribute("style", `--top: ${this.pos.y}px; --left: ${this.pos.x}px; --radius: ${this.radius}px; background-color: rgb(${this.color*255}, ${this.color*255}, ${this.color*255});`)
 	}
+	/**
+	 * Applies gravity to the player
+	 * @param {Player} player The player to apply gravity to
+	 */
 	gravity(player) {
 		var thingy1 = player
 		var thingy2 = this
@@ -34,6 +55,9 @@ class Planet {
 		thingy1.velocity.x += force_strength * direction.x / thingy1.mass
 		thingy1.velocity.y += force_strength * direction.y / thingy1.mass
 	}
+	/**
+	 * Checks for a collision with the player
+	 */
 	checkCollision() {
 		var thingy1 = player
 		var thingy2 = this
@@ -50,6 +74,9 @@ class Player extends Planet {
 	constructor(x, y) {
 		super(x, y, 10, 10, 0.7)
 	}
+	/**
+	 * Updates the player on the screen
+	 */
 	tick() {
 		if (this.velocity.x >  500) this.velocity.x = 0
 		if (this.velocity.x < -500) this.velocity.x = 0
@@ -62,7 +89,8 @@ class Player extends Planet {
 		this.pos.x += this.velocity.x
 		this.pos.y += this.velocity.y
 		this.update()
-		document.body.setAttribute("style", `--top: ${this.pos.y}px;`)
+		//document.body.setAttribute("style", `--top: ${this.pos.y}px;`)
+		window.scrollTo(0, this.pos.y - window.innerHeight / 2)
 	}
 }
 class Bullet extends Player {
@@ -78,11 +106,20 @@ class Bullet extends Player {
 		this.update()
 	}
 }
+// I like Python.
+const random = {
+	random: () => Math.random(),
+	randint: (a, b) => Math.floor(Math.random() * (b - a + 1)) + a,
+	choice: (a) => a[Math.floor(Math.random() * a.length)]
+}
+
 var MOUSE_X = 0
 var MOUSE_Y = 0
 var doGravity = true
 var max_height = 0
 var planets = []
+//          X    Y    RAD  MASS BRIGHT
+//          |    |     |     |   |
 new Planet(408, 396,  250, 3000, 0)
 new Planet(889, 525,  100, 1000, 0)
 new Planet(731, 895,  140, 100,  0)
@@ -90,10 +127,14 @@ new Planet(334, 1120, 10,  9000, 0)
 new Planet(750, 1250, 100, 100,  0)
 new Planet(775, 1675, 300, 100,  0)
 new Planet(600, 2150, 100, 400,  0)
-new Planet(450, 2170, 100, 200,  0)
-new Planet(200, 2190, 100, 300,  0)
+new Planet(450, 2170, 100, 200,  0) //       X POSITION                   Y POSITION                 RADIUS                     MASS         BRIGHTNESS
+new Planet(200, 2190, 100, 300,  0) //   _____________________    ________________________    ____________________    _____________________   |
+var xh = max_height + 10 //             /                     \  /                        \  /                    \  /                     \  v
+for (var i = 0; i < 30; i++) new Planet(random.randint(0, 1275), random.randint(xh, xh * 2), random.randint(0, 250), random.randint(0, 1500), 0)
 var player = new Player(103, 243)
-//player.velocity.y = 1
+/**
+ * Moves one frame ahead in the game
+ */
 function tick() {
 	if (doGravity) {
 		for (var i = 0; i < planets.length; i++) {
@@ -110,7 +151,6 @@ function tick() {
 }
 window.addEventListener("mousemove", (e) => {
 	if (e.buttons == 1) {
-		console.log(e.pageX, e.pageY)
 		player.pos = new Position(e.clientX, e.clientY)
 		player.update()
 	}
